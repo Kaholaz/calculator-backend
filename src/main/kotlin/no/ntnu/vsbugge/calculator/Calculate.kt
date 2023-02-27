7869bc949971d0eb630ada31176aa13f52ce0722
Kaholaz <vkbugge@hotmail.com>
2023-02-28 00:27:16 +0100

    Implement a simple calculator RESTful API.

---
+++ src/main/kotlin/no/ntnu/vsbugge/calculator/Calculate.kt
@@ -0,0 +1,78 @@
+package no.ntnu.vsbugge.calculator
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+// Models
+data class Operation(val a: Double, val b: Double, val operator: Char)
+data class Result(val result: Double)
+
+
+// Services
+/**
+ * Service for performing arithmetic operations
+ */
+@Service
+class ArithmeticService {
+    fun calculate(operation: Operation): Result {
+        return when (operation.operator) {
+            '+' -> Result(operation.a + operation.b)
+            '-' -> Result(operation.a - operation.b)
+            '*' -> Result(operation.a * operation.b)
+            '/' -> Result(operation.a / operation.b)
+            else -> throw IllegalArgumentException("Unknown operator ${operation.operator}")
+        }
+    }
+}
+
+// Controllers
+/**
+ * Controller for the calculator
+ */
+@RestController
+class CalculateController {
+    private val logger = LoggerFactory.getLogger(CalculateController::class.java)
+    private val arithmeticService = ArithmeticService()
+
+    /**
+     * Calculate the result of an operation
+     * @param operation The operation to calculate
+     * @return The result of the operation
+     */
+    @PostMapping("/calculate")
+    fun calculate(@RequestBody operation: Operation): Result {
+        logger.info("Calculating ${operation.a} ${operation.operator} ${operation.b}")
+        return arithmeticService.calculate(operation)
+    }
+}
+
+// Application
+@SpringBootApplication
+class CalculatorApplication
+
+/**
+ * Main function for the calculator
+ */
+fun main(args: Array<String>) {
+    runApplication<CalculatorApplication>(*args)
+}
+
+// Configuration
+@Configuration
+class CorsConfiguration: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        // Very permissive CORS configuration
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+    }
+}
---
+++ src/test/kotlin/no/ntnu/vsbugge/calculator/TestCalculate.kt
@@ -0,0 +1,39 @@
+package no.ntnu.vsbugge.calculator
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+/**
+ * Test the calculator
+ */
+class TestCalculate {
+    private val arithmeticService = ArithmeticService()
+
+    @Test
+    fun testAddition() {
+        val operation = Operation(1.0, 2.0, '+')
+        val result = arithmeticService.calculate(operation)
+        assertEquals(3.0, result.result)
+    }
+
+    @Test
+    fun testSubtraction() {
+        val operation = Operation(1.0, 2.0, '-')
+        val result = arithmeticService.calculate(operation)
+        assertEquals(-1.0, result.result)
+    }
+
+    @Test
+    fun testMultiplication() {
+        val operation = Operation(1.0, 2.0, '*')
+        val result = arithmeticService.calculate(operation)
+        assertEquals(2.0, result.result)
+    }
+
+    @Test
+    fun testDivision() {
+        val operation = Operation(1.0, 2.0, '/')
+        val result = arithmeticService.calculate(operation)
+        assertEquals(0.5, result.result)
+    }
+}
